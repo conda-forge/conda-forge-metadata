b42a2e4eb1fcb8b6586d234d64d350b104d56df1
beckermr <becker.mr@gmail.com>
2023-04-06 06:28:23 -0500

    DOC clarify name

--- conda_forge_metadata/libcfgraph.py
+++ conda_forge_metadata/libcfgraph.py
@@ -140,7 +140,9 @@ def get_libcfgraph_pkgs_for_import(import_name):
         Will return `None` if the import was not found.
     found_import_name : str
         The import name found in the libcfgraph metadata. Only
-        valid if `packages` is not None.
+        valid if `packages` is not None. This name will always be the
+        top-level import with all subpackages removed (e.g., foo.bar.baz
+        will be returned as foo).
     """
     while True:
         supplying_pkgs = _get_libcfgraph_pkgs_for_import(import_name)
