4dc8db1213411b62c99f0e7d7aa68549f58393ac
beckermr <becker.mr@gmail.com>
2024-03-07 16:04:44 -0600

    BUG make sure to have top-level import

--- conda_forge_metadata/autotick_bot/__init__.py
+++ conda_forge_metadata/autotick_bot/__init__.py
@@ -1,2 +1,2 @@
-from .import_to_pkg import map_import_to_package  # noqa
+from .import_to_pkg import map_import_to_package, get_pkgs_for_import  # noqa
 from .pypi_to_conda import get_pypi_name_mapping, map_pypi_to_conda  # noqa
