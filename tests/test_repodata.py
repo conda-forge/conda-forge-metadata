13fc1f7f9f83537df35fcbf3db7c0b78e4ca40a0
jaimergp <jaimergp@users.noreply.github.com>
2024-03-09 16:09:52 +0100

    pre-commit

--- tests/test_repodata.py
+++ tests/test_repodata.py
@@ -1,7 +1,7 @@
 from conda_forge_metadata import repodata
 
 
-def test_labels_anaconda_org(monkeypatch): # type: ignore
+def test_labels_anaconda_org(monkeypatch):  # type: ignore
     monkeypatch.delenv("BINSTAR_TOKEN", raising=False)
     labels = repodata.all_labels()
     assert len(labels) >= 20
