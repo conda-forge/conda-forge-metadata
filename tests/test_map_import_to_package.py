80823ffa7893ef4d9b446909f69e15283d715c6c
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-09-05 07:08:01 -0500

    Update test_map_import_to_package.py

--- tests/test_map_import_to_package.py
+++ tests/test_map_import_to_package.py
@@ -9,7 +9,7 @@ def test_map_import_to_package():
     assert map_import_to_package("numpy.linalg") == "numpy"
 
     # something bespoke
-    assert map_import_to_package("eastlake") == "des-eastlake"
+    assert map_import_to_package("sxdes") == "des-sxdes"
 
     assert map_import_to_package("scipy") == "scipy"
 
@@ -26,10 +26,10 @@ def test_get_pkgs_for_import():
     assert "numpy" in pkgs
 
     # something bespoke
-    pkgs, nm = get_pkgs_for_import("eastlake")
+    pkgs, nm = get_pkgs_for_import("sxdes")
     assert pkgs is not None
-    assert nm == "eastlake"
-    assert "des-eastlake" in pkgs
+    assert nm == "sxdes"
+    assert "des-sxdes" in pkgs
     assert len(pkgs) == 1
 
     pkgs, nm = get_pkgs_for_import("scipy")
